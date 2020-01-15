75f8dcbf37e35352ba30730bce2147df5c8a3723
cheungky <kycheung1985@gmail.com>
2020-01-16 00:10:42 +0800

    temp backup

--- src/main/java/com/test/VMtranslator/Parser.java
+++ src/main/java/com/test/VMtranslator/Parser.java
@@ -5,11 +5,25 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+enum  COMMAND_TYPE {
+    C_ARITHMETIC,
+    C_PUSH,
+    C_POP,
+    C_LABEL,
+    C_GOTO,
+    C_IF,
+    C_FUNCTION,
+    C_RETURN,
+    C_CALL,
+    C_INVALID
+};
+
 public class Parser {
 
   //  private String m_vmFileName;
     private String m_Arg1;
     private int m_Arg2;
+    private COMMAND_TYPE m_CmdType;
     private BufferedReader m_reade;
     private String m_current_instr;
     private boolean m_haveMoreCmd;
@@ -20,10 +34,23 @@ public class Parser {
         m_haveMoreCmd=true;
     }
 
+    private COMMAND_TYPE convertCMDStrToEun(String cmStr){
+        if(cmStr.equals("push")){
+            return COMMAND_TYPE.C_PUSH;
+        }
+
+        if(cmStr.equals("pop")){
+            return COMMAND_TYPE.C_POP;
+        }
+
+        return COMMAND_TYPE.C_INVALID;
+
+    }
+
     void advance() throws IOException{
 
         while(true) {
-            String commandType=null;
+           // String commandType=null;
            // String arg1=null;
            // String arg2=null;
             m_current_instr = m_reade.readLine();
@@ -40,7 +67,7 @@ public class Parser {
                 continue; // if only comment and space skip this line
             }
             if(tokens.length>0) {
-                commandType = tokens[0];
+                m_CmdType = convertCMDStrToEun(tokens[0]);
             }
             if(tokens.length>1) {
                 m_Arg1 = tokens[1];
@@ -53,6 +80,14 @@ public class Parser {
         }
 
 
+    }
+
+    public String getArg1(){
+        return this.m_Arg1;
+    }
+
+    public int getArg2(){
+        return this.m_Arg2;
     }
 
     public void close() throws IOException{
